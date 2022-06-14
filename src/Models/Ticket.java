cffe185c4b0832454ec092ec850d7d9530db64bd
kkurylek <kurylek98@gmail.com>
2022-06-14 19:44:22 +0200

    Added Ticket constructor

--- src/Models/Ticket.java
+++ src/Models/Ticket.java
@@ -4,6 +4,17 @@ import Enums.EnumTicketType;
 import java.math.BigDecimal;
 
 public class Ticket {
+    private static final BigDecimal REUCED_TICKET_MULTIPLIER = new BigDecimal("0.75");
     private EnumTicketType ticketType;
     private BigDecimal price;
+
+    public Ticket(EnumTicketType ticketType, BigDecimal price) {
+        this.ticketType = ticketType;
+
+        if(ticketType == EnumTicketType.NORMAL) {
+            this.price = price;
+        }else {
+            this.price = price.multiply(REUCED_TICKET_MULTIPLIER);
+        }
+    }
 }
